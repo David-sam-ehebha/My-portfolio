ce05ee0e49215a82acf706a3d44078e166821d61
David sam <davesam408@gmail.com>
2026-05-18 15:56:50 +0100

    Import cn helper in AiProjectAssistant to fix build

--- src/components/portfolio/AiProjectAssistant.tsx
+++ src/components/portfolio/AiProjectAssistant.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 import { Sparkles, Copy, RefreshCw, Wand2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
